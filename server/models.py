3998cf75766bc87f0cadf160c746d484d1d54e56
Kelvin Machira <machirakelvin81@gmail.com>
2024-07-06 13:40:57 +0300

    pizza _restaurant

--- server/models.py
+++ server/models.py
@@ -19,7 +19,9 @@ class Restaurant(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     address = db.Column(db.String)
-
+    restaurant_pizzas = db.relationship('RestaurantPizza', back_populates='restaurant')
+    pizzas = association_proxy('restaurant_pizzas', 'pizza')
+    serialize_rules = ('-restaurant_pizzas.restaurant',)
     # add relationship
 
     # add serialization rules
@@ -34,6 +36,9 @@ class Pizza(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     ingredients = db.Column(db.String)
+    restaurant_pizzas = db.relationship('RestaurantPizza', back_populates='pizza')
+    restaurants = association_proxy('restaurant_pizzas', 'restaurant')
+    serialize_rules = ('-restaurant_pizzas.pizza',)
 
     # add relationship
 
@@ -48,7 +53,18 @@ class RestaurantPizza(db.Model, SerializerMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer, nullable=False)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'), nullable=False)
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'), nullable=False)
+    restaurant = db.relationship('Restaurant', back_populates='restaurant_pizzas')
+    pizza = db.relationship('Pizza', back_populates='restaurant_pizzas')
+    
+    @validates('price')
+    def validate_price(self, key, price):
+        if price < 1 or price > 30:
+            raise ValueError('Price must be between 1 and 30')
+        return price
 
+    serialize_rules = ('-restaurant.restaurant_pizzas', '-pizza.restaurant_pizzas')
     # add relationships
 
     # add serialization rules
